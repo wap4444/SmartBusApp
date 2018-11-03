470b931acd646a684e42334c185ad11ac50ea356
wap4444 <wap4444@mail.ru>
2018-11-04 02:48:54 +0600

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -43,32 +43,9 @@ $('.loader1').fadeIn();
 
 
 $("#BtnEnt" ).click(function() {
-var ref = cordova.InAppBrowser.open('http://elecor.controlsoft.kz/app/?push='+localStorage.ipush, '_blank', 'location=no,toolbar=no,disallowoverscroll=yes');
+var ref = cordova.InAppBrowser.open('http://smartbus.controlsoft.kz', '_blank', 'location=no,toolbar=no,disallowoverscroll=yes');
 });
-        
-function didReceiveRemoteNotificationCallBack(jsonData) {}
-function didOpenRemoteNotificationCallBack(jsonData) {}       
-        //Настройка ПУШЕЙ ДЛЯ АЙФОНА
-        var iosSettings = {};
-        iosSettings["kOSSettingsKeyAutoPrompt"] = true;
-        iosSettings["kOSSettingsKeyInAppLaunchURL"] = true;
-
-        //ПОДКЛЮЧЕНИЕ ПУШЕЙ 
-           window.plugins.OneSignal
-          .startInit("52fd2532-f4de-48bd-a818-c918662346cd")
-          .handleNotificationReceived(didReceiveRemoteNotificationCallBack)
-          .handleNotificationOpened(didOpenRemoteNotificationCallBack)
-		  .inFocusDisplaying(window.plugins.OneSignal.OSInFocusDisplayOption.None)
-          .iOSSettings(iosSettings)
-          .endInit();
-        
-window.plugins.OneSignal.getIds(function(ids) {
-ipush = ids.userId;
-$('.loader1').hide();
-localStorage.ipush=ipush;
-var ref = cordova.InAppBrowser.open('http://elecor.controlsoft.kz/app/?push='+ipush, '_blank', 'location=no,toolbar=no,disallowoverscroll=yes');
-$('.loader2').show();
-});
+var ref = cordova.InAppBrowser.open('http://smartbus.controlsoft.kz', '_blank', 'location=no,toolbar=no,disallowoverscroll=yes');
         
 
     }
